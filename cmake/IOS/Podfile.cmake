3ee289d7b12b31a511dba4c748fd88c712bfddb7
QuentinArguillere <quentin.arguillere@belledone-communications.com>
2022-05-23 18:07:52 +0200

    First attempt to include Push tests on a physical device in the CI

--- cmake/IOS/Podfile.cmake
+++ cmake/IOS/Podfile.cmake
@@ -1,12 +1,17 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'LinphoneTester' do
   # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
   use_frameworks!
 
   # Pods for LinphoneTester
-  pod 'linphone-sdk', :path => "@LINPHONESDK_BUILD_DIR@/"
+if ENV['PODFILE_PATH'].nil?
+    pod 'linphone-sdk/swift', :path => "@LINPHONESDK_BUILD_DIR@/"
+else
+    pod 'linphone-sdk/swift', :path => ENV['PODFILE_PATH']  # local sdk
+end
+
 
   target 'LinphoneTesterTests' do
     inherit! :search_paths
